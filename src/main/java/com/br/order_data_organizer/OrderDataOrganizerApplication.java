9a7f144b92d9fabff24e367a34a5981e08608bd5
CarlosBotelho42 <carlospiece55@gmail.com>
2024-12-04 13:25:26 -0300

    Initial commit! xD

---
+++ src/main/java/com/br/order_data_organizer/OrderDataOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.br.order_data_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDataOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderDataOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/order_data_organizer/OrderDataOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.order_data_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDataOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
