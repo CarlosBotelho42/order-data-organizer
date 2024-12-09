fa77928f3d8dc6b32ac02bfff8c77ea067dc0176
CarlosBotelho42 <carlospiece55@gmail.com>
2024-12-09 16:51:22 -0300

    Agora todas a exceptions foram tratadas, oficialmente finalizdo!!!!

---
+++ src/main/java/com/br/order_data_organizer/util/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.br.order_data_organizer.util;
+
+import com.br.order_data_organizer.exception.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<String> handleOrdersNotFoundException(OrderNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro inesperado.");
+    }
+}
